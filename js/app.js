ef28b5a969db40578ee521b807ed4a6603cd69a2
Diego Diniz <83314316+DiegoADP@users.noreply.github.com>
2021-09-10 19:32:25 -0300

    Feat: Close accordion panel when another one opens

--- js/app.js
+++ js/app.js
@@ -1,13 +1,20 @@
 let accordion = document.getElementsByClassName("accordion");
 
 for (let i = 0; i < accordion.length; i++) {
-  accordion[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var accordionContent = this.nextElementSibling;
-    if (accordionContent.style.maxHeight) {
-      accordionContent.style.maxHeight = null;
-    } else {
-      accordionContent.style.maxHeight = accordionContent.scrollHeight + "px";
-    }
-  });
+    accordion[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var accordionContent = this.nextElementSibling;
+        if (accordionContent.style.maxHeight) {
+            accordionContent.style.maxHeight = null;
+        } else {
+            accordionContent.style.maxHeight = accordionContent.scrollHeight + "px";
+        }
+        // Close accordion panel when another one opens
+        for (j = 0; j < accordion.length; j++) {
+            if (j !== i) {
+                accordion[j].nextElementSibling.style.maxHeight = null;
+                accordion[j].classList.remove("active");
+            }
+        }
+    });
 }
