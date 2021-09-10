6854563d223a554b3ddddb7339fff14275c9b16e
Diego Diniz <83314316+DiegoADP@users.noreply.github.com>
2021-09-10 19:00:29 -0300

    Perf: Improve accordion slide dow animation

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
 let accordion = document.getElementsByClassName("accordion");
 
 for (let i = 0; i < accordion.length; i++) {
-    accordion[i].onclick = function () {
-        this.classList.toggle("active");
-        this.nextElementSibling.classList.toggle("show");
-    };
+  accordion[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var accordionContent = this.nextElementSibling;
+    if (accordionContent.style.maxHeight) {
+      accordionContent.style.maxHeight = null;
+    } else {
+      accordionContent.style.maxHeight = accordionContent.scrollHeight + "px";
+    }
+  });
 }
